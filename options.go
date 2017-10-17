8d14ae91e951fbf935602e0c894dba39b8a4b405
zwc <zwczou@gmail.com>
2017-10-17 18:35:39 +0800

    不能忽略apns false

--- options.go
+++ options.go
@@ -4,7 +4,7 @@ type Options struct {
 	SendNo          int    `json:"sendno,omitempty"`
 	TimeLive        int    `json:"time_to_live,omitempty"`
 	OverrideMsgId   int64  `json:"override_msg_id,omitempty"`
-	ApnsProduction  bool   `json:"apns_production,omitempty"`
+	ApnsProduction  bool   `json:"apns_production"`
 	ApnsCollapseId  string `json:"apns_collapse_id,omitempty"`
 	BigPushDuration int    `json:"big_push_duration,omitempty"`
 }
